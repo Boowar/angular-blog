169e9feaf12f80915ec852d97a00a213c22f11d7
boowar <as.yarchuk@gmail.com>
2019-10-08 16:58:08 +0200

    Setting firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
-};
+import { Enviroment } from "./interface"
+
+export const environment: Enviroment = {
+  production: true,
+  apiKey: "AIzaSyAhdFJgnZp3IVSJQlUKHj3Hsy6-mbJnsMs",
+}
