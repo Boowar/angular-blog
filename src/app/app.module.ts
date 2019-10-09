90d6f013a79124cd347601c1aebf779e1ed06f4d
boowar <as.yarchuk@gmail.com>
2019-10-09 15:28:38 +0200

    RU locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { BrowserModule } from "@angular/platform-browser"
+import { Provider } from "@angular/compiler/src/compiler_facade_interface"
+import { NgModule } from "@angular/core"
+import { HTTP_INTERCEPTORS } from "@angular/common/http"
+import { registerLocaleData } from "@angular/common"
+import ruLocale from "@angular/common/locales/ru"
+
 import { AuthInterceptor } from "./shared/auth.interceptor"
 import { SharedModule } from "./shared/shared.module"
-import { BrowserModule } from "@angular/platform-browser"
-import { NgModule } from "@angular/core"
-
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { MainLayoutComponent } from "./shared/components/main-layout/main-layout.component"
 import { HomePageComponent } from "./home-page/home-page.component"
 import { PostPageComponent } from "./post-page/post-page.component"
 import { PostComponent } from "./shared/components/post/post.component"
-import { Provider } from "@angular/compiler/src/compiler_facade_interface"
-import { HTTP_INTERCEPTORS } from "@angular/common/http"
+
+registerLocaleData(ruLocale, "ru")
 
 const INTERCEPTOR_PROVIDER: Provider = {
   provide: HTTP_INTERCEPTORS,
