cb6a89c9b09682f32150ee2e792c954314e012bb
boowar <as.yarchuk@gmail.com>
2019-10-09 15:43:58 +0200

    Posts output

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Post } from "src/app/shared/interfaces"
+import { Component, OnInit } from "@angular/core"
+import { PostsService } from "../shared/posts.service"
+import { Observable } from "rxjs"
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  selector: "app-home-page",
+  templateUrl: "./home-page.component.html",
+  styleUrls: ["./home-page.component.scss"],
 })
 export class HomePageComponent implements OnInit {
+  posts$: Observable<Post[]>
 
-  constructor() { }
+  constructor(private postsService: PostsService) {}
 
   ngOnInit() {
+    this.posts$ = this.postsService.getAll()
   }
-
 }
