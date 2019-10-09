e1dd79c74c80a6d06a6b7cf2d20105e219acc1ce
boowar <as.yarchuk@gmail.com>
2019-10-09 12:02:50 +0200

    Remove post

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -11,21 +11,29 @@ import { Subscription } from "rxjs"
 export class DashboardPageComponent implements OnInit, OnDestroy {
   posts: Post[] = []
   pSub: Subscription
+  dSub: Subscription
   searchStr: string = ""
 
   constructor(private postsService: PostsService) {}
 
   ngOnInit() {
-    this.postsService.getAll().subscribe(posts => {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
       this.posts = posts
     })
   }
 
-  remove(id: string) {}
+  remove(id: string) {
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id)
+    })
+  }
 
   ngOnDestroy() {
     if (this.pSub) {
       this.pSub.unsubscribe()
+    }
+    if (this.dSub) {
+      this.dSub.unsubscribe()
     }
   }
 }
