4bfd8835b2bc9ef72c8ff3523c4b4df4eb1eb342
boowar <as.yarchuk@gmail.com>
2019-10-09 13:37:17 +0200

    Post edit page

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms"
+import { PostsService } from "./../../shared/posts.service"
+import { Component, OnInit } from "@angular/core"
+import { ActivatedRoute, Params } from "@angular/router"
+import { switchMap } from "rxjs/operators"
+import { Post } from "../../shared/interfaces"
 
 @Component({
-  selector: 'app-edit-page',
-  templateUrl: './edit-page.component.html',
-  styleUrls: ['./edit-page.component.scss']
+  selector: "app-edit-page",
+  templateUrl: "./edit-page.component.html",
+  styleUrls: ["./edit-page.component.scss"],
 })
 export class EditPageComponent implements OnInit {
+  form: FormGroup
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) {}
 
   ngOnInit() {
+    this.route.params
+      .pipe(
+        switchMap((params: Params) => {
+          return this.postsService.getById(params["id"])
+        })
+      )
+      .subscribe((post: Post) => {
+        this.form = new FormGroup({
+          title: new FormControl(post.title, Validators.required),
+          text: new FormControl(post.text, Validators.required),
+        })
+      })
   }
 
+  submit() {}
 }
--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -2,7 +2,7 @@ import { Post, FbCreateResponse } from "./interfaces"
 import { HttpClient } from "@angular/common/http"
 import { Injectable } from "@angular/core"
 import { Observable } from "rxjs"
-import { environment } from "src/environments/environment.prod"
+import { environment } from "../../environments/environment.prod"
 import { map } from "rxjs/operators"
 
 @Injectable({ providedIn: "root" })
@@ -29,6 +29,18 @@ export class PostsService {
           id: key,
           date: new Date(response[key].date),
         }))
+      })
+    )
+  }
+
+  getById(id: string): Observable<Post> {
+    return this.http.get<Post>(`${environment.fbDBUrl}/posts/${id}.json`).pipe(
+      map((post: Post) => {
+        return {
+          ...post,
+          id,
+          date: new Date(post.date),
+        }
       })
     )
   }
